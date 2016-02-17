6223c45256f6599ed26517504bed0d3c39b13587
Jonathan <jonacom@lissismore.com>
2016-02-17 08:48:35 -0700

    finished, need to separate to dif files

--- people_database/person.rb
+++ people_database/person.rb
@@ -20,9 +20,44 @@ class PeopleDatabase
   def add(person_data)
     people << Person.new(person_data)
   end
+
+  def remove_by_email(email)
+    people.delete_if do |person|
+      person.email == email
+    end
+
+  end
+
+  def return_all_by_state(state)
+    people.select do |person|
+      person.state == state
+    end
+  end
+
+  def return_all_emails
+    people.map do |person|
+      person.email
+    end.join(", ")
+  end
+
+  def count_all_from_state(state)
+    return_all_by_state(state).count
+  end
+
 end
+a = PeopleDatabase.new([Person.new(first_name: "jim", last_name: "jones", email: "123@gmail", state: "CO")])
 
-a = PeopleDatabase.new([Person.new(first_name: "jim", last_name: "jones", email: "@gmail", state: "CO")])
+a.add(first_name: "jim", last_name: "fine", email: "456@gmail", state: "PA")
+a.add(first_name: "susan", last_name: "fine", email: "478@gmail", state: "PA")
 
-a.add(first_name: "jim", last_name: "jones", email: "@gmail", state: "CO")
-puts a.people.first.first_name 
+# puts a.people
+
+# a.remove_by_email("456@gmail")
+
+# puts a.people
+
+# puts a.return_all_by_state("CO").first.state
+
+# puts a.return_all_emails
+
+puts a.count_all_from_state("PA")
