bf18e82e6e2db773955372a0802db1b45c3a11d3
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-23 08:57:44 -0700

    any method

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,25 +1,13 @@
 class Array
   def any?(&block)
-    x = -1
-    while x < self.length do
-      x += 1
-      if block.call
-        return true
-      end
-      false
+    each do |element|
+      return true if block.call(element)
     end
-
+    false
   end
 end
 
 
 ages = [12, 11, 13, 18, 14,15, 20]
-ages.any? { |age| age > 18 }
-# puts ages.each do |age|
-#
-#   if age > 18
-#     puts "found 18"
-#     return true
-#   end
-#   false
-# end
+
+p ages.any? { |age| age == 21 }
