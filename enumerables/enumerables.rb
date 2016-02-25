d17399696594259e00757e1f767bfc13e95e3bf2
Jonathan <jonacom@lissismore.com>
2016-02-25 08:40:56 -0700

    finished errrthang

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -21,6 +21,14 @@ class Array
     end
     result
   end
+
+  def reject(&block)
+    result = []
+    each do |element|
+      result << element if !block.call(element)
+    end
+    result
+  end
 end
 
 
@@ -29,3 +37,4 @@ ages = [12, 11, 13, 18, 14,15, 20]
 p ages.any? { |age| age == 21 }
 p ages.select { |age| age > 14 }
 p ages.reduce(0) { |sum, num|  sum + num }
+p ages.reject { |num| num % 2 != 0 }
